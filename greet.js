7426822e6a4d6c024631761cf7af523a0a0f146e
Sepler Gábor <sepler.gabor@gmail.com>
2017-06-07 10:17:03 +0200

    a5

--- greet.js
+++ greet.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const greet = function() {
-  console.log('Greetings!');
+const greet = function(name1, name2) {
+  console.log('Greetings ' + name1 + ' and ' + name2 + '!');
 }
 
-greet();
+greet('Dóra', 'Balázs');
